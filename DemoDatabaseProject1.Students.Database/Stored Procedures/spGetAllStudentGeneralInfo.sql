e78d48f0c33b2db78a98f669cc808e74584025fd
mhamner <37221769+mhamner@users.noreply.github.com>
2021-06-07 17:14:24 -0400

    Add files via upload

---
+++ DemoDatabaseProject1.Students.Database/Stored Procedures/spGetAllStudentGeneralInfo.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spGetAllStudentGeneralInfo]
+AS
+	SELECT FirstName, 
+	LastName, 
+	AddressLine1, 
+	AddressLine2, 
+	City,
+	State,
+	Zip,
+	Phone, 
+	Email
+	from dbo.StudentGeneralInfo
+
---
+++ DemoDatabaseProject1.Students.Database/dbo/Tables/StudentGeneralInfo.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[StudentGeneralInfo]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NULL, 
+    [LastName] VARCHAR(50) NULL, 
+    [AddressLine1] VARCHAR(100) NULL, 
+    [AddressLine2] VARCHAR(100) NULL, 
+    [City] VARCHAR(50) NULL, 
+    [State] VARCHAR(50) NULL, 
+    [Zip] VARCHAR(50) NULL, 
+    [Phone] VARCHAR(50) NULL, 
+    [Email] VARCHAR(100) NULL, 
+    [Major] VARCHAR(50) NULL
+)
